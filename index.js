5feb751bed655103fbcea2d6b4d9ba973e26f763
eqram1 <abmeqramhossain@gmail.com>
2023-07-21 07:01:46 +0600

    Load services data from mongodb

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -14,28 +14,30 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.o7ia8pd.mongodb.net/?retryWrites=true&w=majority`;
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+    useNewurlparser: true, useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        const serviceCollection = client.db('servicesCar').collection('services');
+        app.get('/services', async (req, res) => {
+
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally {
+
+
     }
 }
-run().catch(console.dir);
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
